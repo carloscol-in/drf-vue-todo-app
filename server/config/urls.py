91e0057b7c7ed7baad11f76b48d3df2c54ced3e1
ccolin <contact@carloscolin.me>
2021-06-20 22:29:40 -0500

    changed all server directory layout

--- server/config/urls.py
+++ server/config/urls.py
@@ -1,4 +1,4 @@
-"""drf URL Configuration
+"""todo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
@@ -26,4 +26,7 @@ urlpatterns = [
     # Simple JWT
     path('api/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+
+    # Tasks
+    path('api/', include(('todo.tasks.urls', 'tasks')), name='tasks')
 ]
