9004629d6a049f4c820483715da0c75ee7744408
ccolin <contact@carloscolin.me>
2021-06-20 21:53:17 -0500

    added tasks viewset

---
+++ todo/tasks/views.py
@@ -0,0 +1,38 @@
+"""Todo views."""
+
+# DRF
+from rest_framework import mixins, viewsets
+from rest_framework.permissions import IsAuthenticated
+
+# Models
+from todo.tasks.models import Task
+
+# Serializers
+from todo.tasks.serializers import TasksModelSerializer
+
+
+class TasksViewSet(
+        mixins.ListModelMixin,
+        viewsets.GenericViewSet,
+    ):
+    """Tasks viewset."""
+
+    serializer_class = TasksModelSerializer
+    lookup_field = 'pk'
+
+
+    def get_queryset(self):
+        """Get the queryset to return some objects."""
+        queryset = Task.objects.all()
+
+        if self.action == 'list':
+            return queryset.filter(is_complete=True)
+
+        return queryset
+
+    def get_permissions(self):
+        """Get permissions for this view."""
+
+        permissions = [IsAuthenticated]
+
+        return [p() for p in permissions]
