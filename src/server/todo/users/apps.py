25c9d71cc763b0b406e269d9b2a2b6a5eb238585
ccolin <contact@carloscolin.me>
2021-07-01 22:12:15 -0500

    added users app config

---
+++ src/server/todo/users/apps.py
@@ -0,0 +1,11 @@
+"""User app"""
+
+# Django
+from django.apps import AppConfig
+
+
+class UsersAppConfig(AppConfig):
+    """User app config"""
+
+    name = 'todo.users'
+    verbose_name = 'Users'
