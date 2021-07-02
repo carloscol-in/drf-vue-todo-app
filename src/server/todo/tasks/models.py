55e7bb82bdf0c4ea9629e0413dd30def653a2296
ccolin <contact@carloscolin.me>
2021-07-01 22:12:02 -0500

    removed todomodel to add it into utils models

--- src/server/todo/tasks/models.py
+++ src/server/todo/tasks/models.py
@@ -5,30 +5,8 @@ from django.db import models
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
-class TodoModel(models.Model):
-    """Todo abstract model.
-    
-    All models in this project will inherit at least from this
-    model."""
-
-    created = models.DateTimeField(
-        'Created at',
-        auto_now_add=True,
-        help_text="Date and time at which the task was created"
-    )
-    modified = models.DateTimeField(
-        'Modified at',
-        auto_now=True,
-        help_text="Date and time at which the task was last modified"
-    )
-
-    class Meta:
-        """Meta option."""
-
-        abstract = True
-
-        get_latest_by = 'created'
-        ordering = ('-created', '-modified')
+# Utilities
+from todo.utils.models import TodoModel
 
 
 class Task(TodoModel):
